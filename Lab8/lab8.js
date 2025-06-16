58a170e84f9830a8d50bb81b8f93f42005e66eeb
PSTY2 <klasss446@gmail.com>
2025-06-16 22:04:44 +0300

    Lab 8 (JS): функція square (+demo)

---
+++ Lab8/lab8.js
@@ -0,0 +1,9 @@
+// Lab 8 – Функція square
+// ----------------------
+// Приймає число > повертає його квадрат
+function square(number) {
+    return number * number;
+}
+
+// Демо:
+console.log('Квадрат 5 =', square(5));   // > 25
