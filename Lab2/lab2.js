5a535b981b6147ec3fadf0fae42a0eac706b1ea4
PSTY2 <klasss446@gmail.com>
2025-06-16 21:36:20 +0300

    Init project: add Lab2 files & README

---
+++ Lab2/lab2.js
@@ -0,0 +1,24 @@
+//1
+var n1 = 2024;
+let n2 = "Hello everybody!";
+const n3 = 5;
+
+//2
+console.log(typeof n1);
+console.log(typeof n2);
+console.log(typeof n3);
+
+//3
+let student = {
+    name: "Michail",
+    age: 18,
+    st: true
+};
+
+//4
+student.university = "KRAUSS";
+console.log(student);
+
+//5
+n2 = 5;
+console.log(typeof n2); 
---
+++ README.md
@@ -0,0 +1,4 @@
+# Java Lab 2  
+Приклад односторінкового застосунку (index.html + lab2.js).
+
+Форк із репозиторія Mr1Shapovalov/LAB_2_JAVA.  
